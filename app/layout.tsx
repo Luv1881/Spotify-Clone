369749ff87b5537cc739f826de4abab7ef2cdfa3
unknown <ajfoucaultmo@mail.usf.edu>
2023-07-29 10:41:53 -0700

    10. Subscribe modal and account page.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,46 @@
-import './globals.css';
 import { Figtree } from 'next/font/google';
+
+import getSongsByUserId from '@/actions/getSongsByUserId';
+import getActiveProductsWithPrices from '@/actions/getActiveProductsWithPrices';
+import Sidebar from '@/components/Sidebar';
+import ToasterProvider from '@/providers/ToasterProvider';
+import UserProvider from '@/providers/UserProvider';
+import ModalProvider from '@/providers/ModalProvider';
+import SupabaseProvider from '@/providers/SupabaseProvider';
+import Player from '@/components/Player';
+
+import './globals.css';
 
 const font = Figtree({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: 'Spotify',
   description:
     '🎵🎧Spotify clone created by ajfm88 using Next.js 13, Tailwind CSS and TypeScript. 🔊🎶',
 };
 
-export default function RootLayout({
+export const revalidate = 0;
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const products = await getActiveProductsWithPrices();
+  const userSongs = await getSongsByUserId();
+
   return (
     <html lang='en'>
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <ToasterProvider />
+        <SupabaseProvider>
+          <UserProvider>
+            <ModalProvider products={products} />
+            <Sidebar songs={userSongs}>{children}</Sidebar>
+            <Player />
+          </UserProvider>
+        </SupabaseProvider>
+      </body>
     </html>
   );
 }
